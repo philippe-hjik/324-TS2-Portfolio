38458e712e27585f26fc3626085a5396dca13211
Philippe Heijkoop <122774959+philippe-hjik@users.noreply.github.com>
2024-12-19 13:55:55 +0100

    delete(vitestConfig): delete jsdom
    pas besoin de simuler un environnement DOM pour l'instant

--- Projet/vitest.config.js
+++ Projet/vitest.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom', // Utilise jsdom pour simuler un environnement DOM si nécessaire
     globals: true,         // Permet d'utiliser directement 'test' et 'expect' sans les importer
   },
 });
